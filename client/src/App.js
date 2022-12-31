008249aa82c7b895c31dac3b09890e1bc62bad0d
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-31 23:29:40 +0300

    define routes in the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
