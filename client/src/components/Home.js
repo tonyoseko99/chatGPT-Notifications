d3b4fee2a096ed1d251ce23a04c514d742136703
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-31 23:30:39 +0300

    Create the home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,49 @@
+import React, { useState } from "react";
+
 const Home = () => {
+  const [message, setMessage] = useState("");
+  const [subscriber, setSubscriber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ message, subscriber });
+    setMessage("");
+    setSubscriber("");
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <nav className="navbar">
+        <h2>Notify</h2>
+      </nav>
+      <main className="homeContainer">
+        <h3>Send notifications to your users</h3>
+        <form
+          className="notification__form"
+          onSubmit={handleSubmit}
+          method="POST"
+        >
+          <label htmlFor="title">Notification Title</label>
+          <textarea
+            rows={5}
+            name="title"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Let the user know that"
+          />
+          <label htmlFor="subscriber">Subscribers</label>
+
+          <select
+            value={subscriber}
+            name="subscriber"
+            onChange={(e) => setSubscriber(e.target.value)}
+          >
+            <option value="Select">Select</option>
+          </select>
+          <button>SEND NOTIFICATION</button>
+        </form>
+      </main>
     </div>
   );
 };
